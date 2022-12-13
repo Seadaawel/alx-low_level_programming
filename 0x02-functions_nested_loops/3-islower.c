4a687d650f3c819883d9deb71c4c3d89c78cf711
Seada Awel <sewdabintaswed@gmail.com>
2022-12-13 13:20:46 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,10 @@
 /**
  * _islower - check for lower case letter
  * @c : character to check the case
- * Return: return 1 or 0 
+ * Return:0 or 1
  */
-int _islower(int c)
+
+int -islower(int c)
 {
 	return (c >= 97 && c <= 122);
 }
