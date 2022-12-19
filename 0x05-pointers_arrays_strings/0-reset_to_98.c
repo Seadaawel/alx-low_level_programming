23f630206d5685df67189dd46e8f1d68886499ed
Seada Awel <sewdabintaswed@gmail.com>
2022-12-19 11:53:37 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "seadi.h"
-#include <stdio.h>
 
 /**
  * reset_to_98n - take a pointer to an int as parameters and updates
  * the value it points to to 98
+ *
  * @n: point to an int
- * Return: Nothing
+ *
  */
 
 void reset_to_98(int *n);
