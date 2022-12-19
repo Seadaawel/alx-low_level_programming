d58083eb0e42cbbfaa6b1b64d064d47eeab982c9
Seada Awel <sewdabintaswed@gmail.com>
2022-12-19 10:40:41 -0800

    main.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - update the points to 98.
+ *
+ * return: Always 0
+ */
+
+int main(void)
+{
+	int x = 98;
+	int *ip;
+
+	ip = &x;
+	printf("the value of ip is : %d\n" x);
+
+	return (0);
+}
